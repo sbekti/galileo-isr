e837b8765fcf7a2d6ea8d0d58d74d2da76ef24f2
Samudra Harapan Bekti <samudra.bekti@gmail.com>
2015-09-25 01:10:07 -0700

    he

--- clock_isr.cc
+++ clock_isr.cc
@@ -1,13 +1,13 @@
 #include <nan.h>
 #include "clock_isr.h"
 
-// volatile uint32_t ticks = 0;
+uint32_t ticks = 0;
 
 NAN_METHOD(GetTime) {
-  info.GetReturnValue().Set(9);
+  info.GetReturnValue().Set(ticks);
 }
 
 NAN_METHOD(SetTime) {
-  int ticks = info[0]->Uint32Value();
+  ticks = info[0]->Uint32Value();
   info.GetReturnValue().Set(ticks);
 }
